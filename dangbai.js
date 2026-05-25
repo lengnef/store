74784320c99bbc4099f5eb767c973ed27917d696
lengnef <lengnef.font@gmail.com>
2026-05-25 08:44:06 +0700

    Update dangbai.js

--- dangbai.js
+++ dangbai.js
@@ -18,7 +18,10 @@ function loadFontIfNeeded(fontName, fontFile) {
 }
 
 const FONTS = [
-
+  { name: "LF Galiano Text",        styles: 1, image: "images/LFGalianoText.jpg",          font: "fonts/LF Galiano Text.ttf",        id: "LF Galiano Text.ttf" },
+  { name: "LF Vermin Verile",        styles: 1, image: "images/LFVerminVerile.jpg",        font: "fonts/LF Vermin Verile.ttf",       id: "LF Vermin Verile.ttf" },
+  { name: "LF Viva La Fiesta",       styles: 1, image: "images/LFVivaLaFiesta.jpg",        font: "fonts/LF Viva La Fiesta - Uno Base.ttf",            id: "LF Viva La Fiesta - Uno Base.ttf" },
+  { name: "LF Axel",                 styles: 1, image: "images/LFAxel.jpg",                font: "fonts/LF Axel Regular.ttf",        id: "LF Axel.zip" },
   { name: "LF Plumage",              styles: 1, image: "images/lfplumage.jpg",             font: "fonts/LF Plumage.ttf",             id: "LF Plumage.ttf" },
   { name: "LF Christmas Fecsival",   styles: 1, image: "images/lfchristmasfecsival.jpg",   font: "fonts/LF Christmas Fecsival.ttf",  id: "LF Christmas Fecsival.ttf" },
   { name: "LF Winstgue",             styles: 1, image: "images/lfwinstgue.jpg",            font: "fonts/LF Winstgue.ttf",            id: "LF Winstgue.ttf" },
