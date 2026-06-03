69d57931a23928729b30510462d9308db74da7e4
lengnef <lengnef.font@gmail.com>
2026-06-03 08:01:07 +0700

    Update dangbai.js

--- dangbai.js
+++ dangbai.js
@@ -18,6 +18,7 @@ function loadFontIfNeeded(fontName, fontFile) {
 }
 
 const FONTS = [
+  { name: "LF Brotherhood Script",   styles: 1, image: "images/LFBrotherhoodScript.jpg",   font: "fonts/LF Brotherhood Script.ttf",  id: "LF Brotherhood Script.ttf" },
   { name: "LF Hatton",               styles: 3, image: "images/LFHatton.jpg",              font: "fonts/LF Hatton Regular.ttf",      id: "LF Hatton.zip" },
   { name: "LF Storm Fighter",        styles: 1, image: "images/LFStormFighter.jpg",        font: "fonts/LF Storm Fighter.ttf",       id: "LF Storm Fighter.ttf" },
   { name: "LF Brutal",               styles: 1, image: "images/LFBrutal.jpg",              font: "fonts/LF Brutal.ttf",              id: "LF Brutal.ttf" },
