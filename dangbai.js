77679d4d50eff00a7e53a28f3ff889256e4bed14
lengnef <lengnef.font@gmail.com>
2026-06-17 10:55:08 +0700

    Update dangbai.js

--- dangbai.js
+++ dangbai.js
@@ -18,6 +18,7 @@ function loadFontIfNeeded(fontName, fontFile) {
 }
 
 const FONTS = [
+  { name: "LF Budge",                styles: 1, image: "images/LFBudge.jpg",               font: "fonts/LF Budge.ttf",               id: "LF Budge.ttf" },
   { name: "LF Splendid Script",      styles: 1, image: "images/LFSplendidScript.jpg",      font: "fonts/LF Splendid Script.ttf",     id: "LF Splendid Script.ttf" },
   { name: "LF Scaver",               styles: 1, image: "images/LFScaver.jpg",              font: "fonts/LF Scaver Regular.ttf",      id: "LF Scaver Regular.ttf" },
   { name: "LF Gosha Sans",           styles: 3, image: "images/LFGoshaSans.jpg",           font: "fonts/LF Gosha Sans.ttf",          id: "LF Gosha Sans.zip" },
