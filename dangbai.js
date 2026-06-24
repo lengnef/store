3d67c8d02ae4f45438ffb401e11f606cd8d54284
lengnef <lengnef.font@gmail.com>
2026-06-24 16:46:12 +0700

    Update dangbai.js

--- dangbai.js
+++ dangbai.js
@@ -18,6 +18,7 @@ function loadFontIfNeeded(fontName, fontFile) {
 }
 
 const FONTS = [
+  { name: "LF Histogram",            styles: 1, image: "images/LFHistogram.jpg",           font: "fonts/LF Histogram.ttf",           id: "LF Histogram.ttf" },
   { name: "LF Chalkcoal",            styles: 1, image: "images/LFChalkcoal.jpg",           font: "fonts/LF Chalkcoal.ttf",           id: "LF Chalkcoal.ttf" },
   { name: "LF Palm Canyon Drive",    styles: 1, image: "images/LFPalmCanyonDrive.jpg",     font: "fonts/LF Palm Canyon Drive.ttf",   id: "LF Palm Canyon Drive.ttf" },
   { name: "LF Masmuseh",             styles: 1, image: "images/LFMasmuseh.jpg",            font: "fonts/LF Masmuseh.ttf",            id: "LF Masmuseh.ttf" },
