ab831a5e4bf547e69dfb5d762d2f8f50c73fe07e
lengnef <lengnef.font@gmail.com>
2026-07-04 09:00:44 +0700

    Update dangbai.js

--- dangbai.js
+++ dangbai.js
@@ -18,6 +18,7 @@ function loadFontIfNeeded(fontName, fontFile) {
 }
 
 const FONTS = [
+  { name: "LF Symphony Pro",         styles: 1, image: "images/LFSymphonyPro.jpg",         font: "fonts/LF Symphony Pro.ttf",        id: "LF Symphony Pro.ttf" },
   { name: "LF Avalen Rekas",         styles: 1, image: "images/LFAvalenRekas.jpg",         font: "fonts/LF Avalen Rekas.ttf",        id: "LF Avalen Rekas.ttf" },
   { name: "LF Histogram",            styles: 1, image: "images/LFHistogram.jpg",           font: "fonts/LF Histogram.ttf",           id: "LF Histogram.ttf" },
   { name: "LF Chalkcoal",            styles: 1, image: "images/LFChalkcoal.jpg",           font: "fonts/LF Chalkcoal.ttf",           id: "LF Chalkcoal.ttf" },
