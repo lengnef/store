27b0c2c0f839e7afe7342ab58ae4a7899c1a64d5
lengnef <lengnef.font@gmail.com>
2026-07-07 13:13:24 +0700

    Update dangbai.js

--- dangbai.js
+++ dangbai.js
@@ -18,6 +18,7 @@ function loadFontIfNeeded(fontName, fontFile) {
 }
 
 const FONTS = [
+  { name: "LF Belanosima",           styles: 1, image: "images/LFBelanosima.jpg",          font: "fonts/LF Belanosima.ttf",          id: "LF Belanosima.ttf" },
   { name: "LF Symphony Pro",         styles: 1, image: "images/LFSymphonyPro.jpg",         font: "fonts/LF Symphony Pro.ttf",        id: "LF Symphony Pro.ttf" },
   { name: "LF Avalen Rekas",         styles: 1, image: "images/LFAvalenRekas.jpg",         font: "fonts/LF Avalen Rekas.ttf",        id: "LF Avalen Rekas.ttf" },
   { name: "LF Histogram",            styles: 1, image: "images/LFHistogram.jpg",           font: "fonts/LF Histogram.ttf",           id: "LF Histogram.ttf" },
