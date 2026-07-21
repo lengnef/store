7ca4185f41e4e3ea99453bf639b269690a7b4129
lengnef <lengnef.font@gmail.com>
2026-07-21 16:52:33 +0700

    Update dangbai.js

--- dangbai.js
+++ dangbai.js
@@ -18,6 +18,7 @@ function loadFontIfNeeded(fontName, fontFile) {
 }
 
 const FONTS = [
+  { name: "LF Minecraft Ten",        styles: 1, image: "images/LFMinecraftTen.jpg",        font: "fonts/LF Minecraft Ten.ttf",       id: "LF Minecraft Ten.ttf" },
   { name: "LF Feinlag",              styles: 1, image: "images/LFFeinlag.jpg",             font: "fonts/LF Feinlag.ttf",             id: "LF Feinlag.ttf" },
   { name: "LF Bi",                   styles: 1, image: "images/LFBi.jpg",                  font: "fonts/LF Bi.ttf",                  id: "LF Bi.ttf" },
   { name: "LF Stardew Valley",       styles: 1, image: "images/LFStardewValley.jpg",       font: "fonts/LF Stardew Valley ALL CAPS.ttf",        id: "LF Stardew Valley ALL CAPS.ttf" },
