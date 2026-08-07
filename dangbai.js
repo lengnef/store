1b6f02183229e0dd6cb51c0b849d273fa5dbce9c
lengnef <lengnef.font@gmail.com>
2026-08-07 18:36:42 +0700

    Update dangbai.js

--- dangbai.js
+++ dangbai.js
@@ -18,7 +18,7 @@ function loadFontIfNeeded(fontName, fontFile) {
 }
 
 const FONTS = [
-  { name: "LF Bolgifam Script",      styles: 1, image: "images/LFBolgifamScript.jpg",     font: "#",                        id: "LF BolgifamScript.ttf" },
+  { name: "LF Bolgifam Script",      styles: 1, image: "images/LFBolgifamScript.jpg",      font: "#",                        id: "#" },
   { name: "LF Cutline",              styles: 1, image: "images/LFCutline.jpg",             font: "LF Cutline.ttf",                   id: "LF Cutline.ttf" },
   { name: "LF Virlyn",               styles: 1, image: "images/LFVirlyn.jpg",              font: "LF Virlyn.ttf",                    id: "LF Virlyn.ttf" },
   { name: "LF Mackin",               styles: 1, image: "images/LFMackin.jpg",              font: "fonts/LF Mackin.ttf",              id: "LF Mackin.ttf" },
