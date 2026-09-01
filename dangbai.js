2693249a4807120b1ad0126ec2336655f961d267
lengnef <lengnef.font@gmail.com>
2026-09-01 09:40:05 +0700

    Update dangbai.js

--- dangbai.js
+++ dangbai.js
@@ -19,6 +19,7 @@ function loadFontIfNeeded(fontName, fontFile) {
 
 const FONTS = [
 
+  { name: "LF Akbaal",               styles: 1, image: "images/LFAkbaal.jpg",              font: "fonts/LF Akbaal.ttf",              id: "LF Akbaal.ttf" },
   { name: "LF Sweet Peachy",         styles: 1, image: "images/LFSweetPeachy.jpg",         font: "fonts/LF Sweet Peachy.ttf",        id: "LF Sweet Peachy.ttf" },
   { name: "LF Negrita Pro",          styles: 1, image: "images/LFNegritaPro.jpg",          font: "#",         id: "#" },
   { name: "LF H.H. Samuel",          styles: 1, image: "images/LFH.H.Samuel.jpg",          font: "fonts/LF H.H. Samuel.ttf",         id: "LF H.H. Samuel.ttf" },
