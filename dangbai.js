29fdfc853eda3a8452da5cbb594dfc4d28233522
lengnef <lengnef.font@gmail.com>
2026-09-23 09:08:38 +0700

    Update dangbai.js

--- dangbai.js
+++ dangbai.js
@@ -20,6 +20,7 @@ function loadFontIfNeeded(fontName, fontFile) {
 const FONTS = [
 
 
+  { name: "LF Thegora",              styles: 1, image: "images/LFThegora.jpg",             font: "#",           id: "#" },
   { name: "LF Bing Boss",            styles: 1, image: "images/LFBingBoss.jpg",            font: "fonts/LF Bing Boss.ttf",           id: "LF Bing Boss.ttf" },
   { name: "LF Discoteca Rounded",    styles: 1, image: "images/LFDiscotecaRounded.jpg",    font: "fonts/LF Discoteca Rounded.ttf",   id: "LF Discoteca Rounded.ttf" },
   { name: "LF Streetbrush",          styles: 1, image: "images/LFStreetbrush.jpg",         font: "fonts/LF Streetbrush.ttf",         id: "LF Streetbrush.ttf" },
